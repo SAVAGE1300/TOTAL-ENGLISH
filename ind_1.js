4b1857853cb47077c53be9fbea234c80cdc595da
SAVAGE <89578700+SAVAGE1300@users.noreply.github.com>
2022-01-17 11:53:51 +0500

    Delete ind_1.js

--- ind_1.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    document.getElementById("container").style.transform = "scale(1.2)"
-    document.getElementById("container").style.transitionDuration = "2s"
-}
-function myFunction1() {
-    document.getElementById("jum4").style.backgroundImage = "linear-gradient(to right, red, black);"
-}
